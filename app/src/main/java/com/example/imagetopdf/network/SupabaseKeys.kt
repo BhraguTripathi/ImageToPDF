c69919b8c6d90ec484da09b6cc2d2c1b6f13b86c
BhraguTripathi <bhragu.12324742@lpu.in>
2026-02-27 13:29:59 +0530

    Delete SupabaseKeys.kt

--- app/src/main/java/com/example/imagetopdf/network/SupabaseKeys.kt
+++
@@ -1,6 +0,0 @@
-package com.example.imagetopdf.network
-
-object SupabaseKeys {
-    const val URL = "https://lupudiimbboecosnqmmo.supabase.co"
-    const val KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1cHVkaWltYmJvZWNvc25xbW1vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxNzIzOTEsImV4cCI6MjA4Nzc0ODM5MX0.6qyCAhQ7sPQoY-EDyMC2Tnp2Zo5hc_A4mHUZXPguhiY"
-}
